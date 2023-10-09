0e45b192a7231e53ebd94a951d94c1401a490619
Daiana Russi <daianarussi.agroconsult@gmail.com>
2023-10-09 12:50:08 -0300

    patterns

--- Service/FilterService.cs
+++ Service/FilterService.cs
@@ -20,10 +20,10 @@ namespace AgroCoordenadas.Service
 
         public FilteredData Filter(string text)
         {
-            string pattern1 = @"(?<=\s+N\s+|\(\s*N\s*|\s*N=\s*)([\d,.]+)";
+   string pattern1 = @"(?<=\s+N\s+|\(\s*N\s*|\s*N=\s*)([\d,.]+)";
             string pattern2 = @"(?<=\s+E\s+|\(\s*E\s*|\s*E=\s*)([\d,.]+)";
-            string pattern3 = @"(?<=\s*latitude|\s*latitude:|\s*Latitude:\s*|\s*Latitude\s*de\s*|\s*Latitude\s*|\s*Lat\s*|\s*Lat:\s*|\(Latitude:\s*|\(Latitude\s*|\(Lat\s*|\(Lat:\s*)([-+°º'”\s\d,.]+)(""|”)";
-            string pattern4 = @"(?<=\s*longitude|\s*longitude:|\s*Longitude:\s*|\s*Longitude\s*de\s*|\s*Longitude\s*|\s*Long\s*|\s*Long:\s*|\(Longitude:\s*|\(Longitude\s*|\(Long\s*|\(Long:\s*)([-+°º'""”\s\d,.]+)(""|”)";
+            string pattern3 = @"(?<=\s*Latitude:\s*|\s*latitude:\s*|\s*latitude\s*|\s*Latitude\s*de\s*|\s*Lat\s*=\s*|\s*Latitude\s*|\s*Lat\s*|\s*Lat:\s*|\(Latitude:\s*|\(Latitude\s*|\(Lat\s*|\(Lat:\s*)([-+°º'”\s\d,.]+)(""|”)";
+            string pattern4 = @"(?<=\s*Longitude:\s*|\s*longitude:\s*|\s*longitude\s*|\s*Longitude\s*de\s*|\s*Long\s*=\s*|\s*Longitude\s*|\s*Long\s*|\s*Long:\s*|\(Longitude:\s*|\(Longitude\s*|\(Long\s*|\(Long:\s*)([-+°º'""”\s\d,.]+)(""|”)";
 
             HashSet<string> seenValues1 = new();
             HashSet<string> seenValues2 = new();
